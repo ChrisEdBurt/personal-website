5bd9612ecfee5af317bf28dc46fff155393aebe7
Chris <chrisedburt@gmail.com>
2024-02-14 16:55:57 +1300

    typo: updated image paths, made footer text easier to read

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import HeroImage from '../../../public/images//hero-image.png';
 
 const HeroSection = () => {
   return (
@@ -46,7 +47,7 @@ const HeroSection = () => {
           className='col-span-4 place-self-center mt-4 lg:mt-0'>
           <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
             <Image
-              src='/images/hero-image.png'
+              src={HeroImage}
               alt='hero'
               className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full'
               width={300}
